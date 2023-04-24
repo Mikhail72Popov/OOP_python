2b7adc9dae54e732ea487bdd2efa449ee29680e8
Михаил Попов <116198836+Mikhail72Popov@users.noreply.github.com>
2023-04-25 00:23:49 +0600

    Delete Warehouse.py

--- Warehouse.py
+++
@@ -1,9 +0,0 @@
-class Warehouse:   
-
-    def __init__(self, product, randomMemory, hardDisk, operSystem):
-        self._product = product
-        self._randomMemory = randomMemory
-        self._hardDisk = hardDisk
-        self._operSystem = operSystem
-
-
